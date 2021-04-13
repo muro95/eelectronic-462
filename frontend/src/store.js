174b38a9d7de3e539c3a34a259c0d4ecf48e2966
muro95 <sonbena95@gmail.com>
2021-04-13 12:43:31 -0700

    add redux to product details screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,13 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 // help return list of products in data.js in frontend
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
-})
+    productDetails: productDetailsReducer,
+});
 //Connect to developer tool 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
