d7919422226eeb0d00cd0ea686d5b96f3b71889a
muro95 <sonbena95@gmail.com>
2021-04-13 12:04:50 -0700

    Adding redux to home page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { useEffect  } from 'react'
 import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-    /* Manage the state of React components */
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    /* useDispatch() help to dispatch any redux action inside react components */
+    const dispatch = useDispatch();
+    // Use useSelector to get object from redux store 
+    const productList = useSelector( state => state.productList);
+    const { loading, error, products } = productList;
+
     useEffect(() =>{
         /* send req to backend */
-        const fecthData = async () =>{
-            try{
-            setLoading(true);
-            const { data } = await axios.get('/api/products');
-            setLoading(false);
-            setProducts(data);  
-            }catch(err){
-                setError(err.message);
-                setLoading(false);
-            }
-        };
-        fecthData();
+        dispatch(listProducts());
         /* this will help run only one time after rendering componenet */
     }, []);
     return (
---
+++ frontend/src/store.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+// help return list of products in data.js in frontend
+const initialState = {};
+const reducer = combineReducers({
+    productList: productListReducer,
+})
+//Connect to developer tool 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer, 
+    initialState, 
+    composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;
