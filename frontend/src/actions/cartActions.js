91b5662b3af521931c5af231afb9b78220f607ee
muro95 <sonbena95@gmail.com>
2021-04-14 13:33:01 -0700

    implement cart delete func

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (productId, qty) => async(dispatch, getState) =>{
     //sending a ajax request to server to get product' info
@@ -16,5 +16,11 @@ export const addToCart = (productId, qty) => async(dispatch, getState) =>{
         },
     });
     //save item to local storage
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+    dispatch({type: CART_REMOVE_ITEM, payload: productId});
+    //update localStorage after remove
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
