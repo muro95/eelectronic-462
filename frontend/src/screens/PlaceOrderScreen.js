8cdaa2af6e967239e44bbe1114c4d3514e5c3d0e
muro95 <sonbena95@gmail.com>
2021-04-16 01:30:19 -0700

    adding place order action

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
 import CheckoutSteps from '../components/CheckoutSteps'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function PlaceOrderScreen(props) {
     const cart = useSelector(state => state.cart);
     if(!cart.paymentMethod){
         props.history.push('/payment');
     }
+    const orderCreate = useSelector(state => state.orderCreate);
+    const { loading, success, error, order } = orderCreate;
     //round to 2 digit after decimal point 
     const toPrice = (num) => Number(num.toFixed(2)); 
     cart.itemsPrice = toPrice(cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0));
     cart.shippingPrice = cart.itemsPrice > 100? toPrice(0) : toPrice(10);
     cart.taxPrice = toPrice(0.15 * cart.itemsPrice)
     cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+    const dispatch = useDispatch();
     const placeOrderHandler = () => {
-
+        //use alluse all fields of card object and replaced card item with order item
+        dispatch(createOrder({...cart, orderItems: cart.cartItems}));
     }
+    useEffect(() => {
+        if(success) {
+            props.history.push(`/order/${order._id}`);
+            dispatch({ type: ORDER_CREATE_RESET});
+        }
+    }, [ dispatch, order, props.history, success]);
     return (
         <div>
             <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
@@ -117,6 +131,10 @@ export default function PlaceOrderScreen(props) {
                                     Place Order
                                 </button>
                             </li>
+                            {
+                                loading && <LoadingBox></LoadingBox>
+                            }
+                            {error && <MessageBox varian="danger">{error}</MessageBox>}
                         </ul>
                     </div>
 
