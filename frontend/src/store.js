506358a844d6003daca8377d1046d94c682ecc6b
muro95 <sonbena95@gmail.com>
2021-04-15 19:05:13 -0700

    adding signIn action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,14 @@ import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
 const initialState = {
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo')
+        ? JSON.parse(localStorage.getItem('userInfo'))
+        : null,
+    },
     cart:{
         //read the content of localStorage, then check the item is exist
         //then use JSON.parse to conver string to array 
@@ -16,6 +22,7 @@ const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userSignin: userSigninReducer,
 });
 //Connect to developer tool 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
