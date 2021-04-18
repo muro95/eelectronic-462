f77a105cf984180e90356f4473b3997f2fca1f23
muro95 <sonbena95@gmail.com>
2021-04-17 18:56:09 -0700

    created admin section

---
+++ frontend/src/components/AdminRoute.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+
+export default function AdminRoute({ component: Component, ...rest }) {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  //private route that only render components if user signin
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        userInfo && userInfo.isAdmin ? (
+          <Component {...props}></Component>
+        ) : (
+          <Redirect to="/signin" />
+        )
+      }
+    ></Route>
+  );
+}
