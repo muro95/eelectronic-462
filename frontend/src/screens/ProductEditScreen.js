856531928989a4f1459165b2421a661862b4f634
muro95 <sonbena95@gmail.com>
2021-04-17 20:27:37 -0700

    creted product edit page

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+export default function ProductEditScreen(props) {
+    const productId = props.match.params.id;
+    //defind hook for product fills 
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [image, setImage] = useState('');
+    const [category, setCategory] = useState('');
+    const [countInStock, setCountInStock] = useState('');
+    const [brand, setBrand] = useState('');
+    const [description, setDescription] = useState('');
+    const dispatch = useDispatch();
+
+    const productDetails = useSelector(state => state.productDetails);
+    const { loading, error, product } = productDetails;
+
+    useEffect(() => {
+        //if product is null then we need to load from backend or 
+        //if product id not equal to new product id req, run detail product again to prevent showing previous info
+        if(!product || (product._id !== productId)){
+            dispatch(detailsProduct(productId));
+        } else {
+            //if it contains value, set the fill with the data from backend 
+            setName(product.name);
+            setPrice(product.price);
+            setImage(product.image);
+            setCategory(product.category);
+            setCountInStock(product.countInStock);
+            setBrand(product.brand);
+            setDescription(product.description);
+        }
+    }, [ product, dispatch,productId]);
+    const submitHandler = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div>
+            <form className="form" onSubmit={submitHandler}>
+                <div>
+                    <h1>Edit Product</h1>
+                </div>
+            {loading ? (
+                    <LoadingBox></LoadingBox>
+            ) : error ? (
+                <MessageBox>{error}</MessageBox>
+            ) : (
+                <>
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input 
+                            id="name"
+                            type="text" 
+                            placeholder="Enter name" 
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label htmlFor="price">Price</label>
+                        <input 
+                            id="price"
+                            type="text" 
+                            placeholder="Enter price" 
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label htmlFor="image">Image</label>
+                        <input 
+                            id="image"
+                            type="text" 
+                            placeholder="Enter image" 
+                            value={image}
+                            onChange={(e) => setImage(e.target.value)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label htmlFor="category">Category</label>
+                        <input 
+                            id="category"
+                            type="text" 
+                            placeholder="Enter category" 
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label htmlFor="brand">Brand</label>
+                        <input 
+                            id="brand"
+                            type="text" 
+                            placeholder="Enter brand" 
+                            value={brand}
+                            onChange={(e) => setBrand(e.target.value)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label htmlFor="countInStock">Count In Stock</label>
+                        <input 
+                            id="countInStock"
+                            type="text" 
+                            placeholder="Enter countInStock" 
+                            value={countInStock}
+                            onChange={(e) => setCountInStock(e.target.value)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description</label>
+                        <textarea
+                            id="description"
+                            rows="3"
+                            type="text" 
+                            placeholder="Enter description" 
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        ></textarea>
+                    </div>
+                    <div>
+                        <label></label>
+                        <button className="primary" type="submit">
+                            Update
+                        </button>
+                    </div>
+                </>
+                )}
+            </form>
+        </div>
+    );
+}
